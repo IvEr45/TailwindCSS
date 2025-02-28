0079171ce692d1e972bafffb246defde73d467cb
IvEr45 <135097522+IvEr45@users.noreply.github.com>
2025-02-28 14:17:34 +0000

    Hello!

---
+++ TailwindCSS/resources/views/tailwind.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/tailwind.css') }}" rel="stylesheet">
+  </head>
+  <body>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+      <div class="text-3xl font-bold text-blue-500"> And Hello, Tailwind!</div>
+    </h1>
+  </body>
+</html>
